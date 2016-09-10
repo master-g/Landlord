5df85cd42f53aa41c199df9e595c8146416714f0
Master.G <dearunclexiao@qq.com>
2016-09-10 18:32:31 +0800

    update

--- src/card.h
+++ src/card.h
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2014 Master.G
+Copyright (c) 2016 Master.G
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -31,29 +31,88 @@ SOFTWARE.
 extern "C" {
 #endif
 
-#define CARD_RANK_3     (uint8_t)0x01
-#define CARD_RANK_4     (uint8_t)0x02
-#define CARD_RANK_5     (uint8_t)0x03
-#define CARD_RANK_6     (uint8_t)0x04
-#define CARD_RANK_7     (uint8_t)0x05
-#define CARD_RANK_8     (uint8_t)0x06
-#define CARD_RANK_9     (uint8_t)0x07
-#define CARD_RANK_T     (uint8_t)0x08
-#define CARD_RANK_J     (uint8_t)0x09
-#define CARD_RANK_Q     (uint8_t)0x0A
-#define CARD_RANK_K     (uint8_t)0x0B
-#define CARD_RANK_A     (uint8_t)0x0C
-#define CARD_RANK_2     (uint8_t)0x0D
-#define CARD_RANK_r     (uint8_t)0x0E
-#define CARD_RANK_R     (uint8_t)0x0F
+/*
+ * here is how a card is represent in a 32 bit unsigned integer
+ * ------------------------------------------------------------------
+ * 32            24 23            16               8  7             0
+ * ------------------------------------------------------------------
+ * x x b b b b b b  b b b b b b b b  c d h s r r r r  x x p p p p p p
+ *
+ * x: reserved bits
+ * b: binary represent of the card's rank
+ * c: club
+ * d: diamond
+ * h: heart
+ * s: spade
+ * r: hex represent of the card's rank
+ * p: prime number represent of the card's rank
+ *
+ * ---
+ *
+ * prime number represent of the card's rank
+ *
+ * prime
+ * 2 3 5 7 11 13 17 19 23 29 31 37 41 43    47
+ * rank
+ * 3 4 5 6 7  8  9  10 J  Q  K  A  2  joker JOKER
+ *
+ * ---
+ *
+ * thus we have
+ */
 
-#define CARD_RANK_BEG   CARD_RANK_3
-#define CARD_RANK_END   (CARD_RANK_R + 1)
+#define CARD_PRIME_3  0x02
+#define CARD_PRIME_4  0x03
+#define CARD_PRIME_5  0x05
+#define CARD_PRIME_6  0x07
+#define CARD_PRIME_7  0x0B
+#define CARD_PRIME_8  0x0D
+#define CARD_PRIME_9  0x11
+#define CARD_PRIME_T  0x13
+#define CARD_PRIME_J  0x17
+#define CARD_PRIME_Q  0x1D
+#define CARD_PRIME_K  0x1F
+#define CARD_PRIME_A  0x25
+#define CARD_PRIME_2  0x29
+#define CARD_PRIME_r  0x2B
+#define CARD_PRIME_R  0x2F
 
-#define CARD_SUIT_CLUB      0x10
-#define CARD_SUIT_DIAMOND   0x20
-#define CARD_SUIT_HEART     0x30
-#define CARD_SUIT_SPADE     0x40
+#define CARD_RANK_3   0x01
+#define CARD_RANK_4   0x02
+#define CARD_RANK_5   0x03
+#define CARD_RANK_6   0x04
+#define CARD_RANK_7   0x05
+#define CARD_RANK_8   0x06
+#define CARD_RANK_9   0x07
+#define CARD_RANK_T   0x08
+#define CARD_RANK_J   0x09
+#define CARD_RANK_Q   0x0A
+#define CARD_RANK_K   0x0B
+#define CARD_RANK_A   0x0C
+#define CARD_RANK_2   0x0D
+#define CARD_RANK_r   0x0E
+#define CARD_RANK_R   0x0F
+
+#define CARD_SUIT_CLUB      0x08
+#define CARD_SUIT_DIAMOND   0x04
+#define CARD_SUIT_HEART     0x02
+#define CARD_SUIT_SPADE     0x01
+
+#define CARD_BIN_3    0x0002
+#define CARD_BIN_4    0x0004
+#define CARD_BIN_5    0x0008
+#define CARD_BIN_6    0x0010
+#define CARD_BIN_7    0x0020
+#define CARD_BIN_8    0x0040
+#define CARD_BIN_9    0x0080
+#define CARD_BIN_T    0x0100
+#define CARD_BIN_J    0x0200
+#define CARD_BIN_Q    0x0400
+#define CARD_BIN_K    0x0800
+#define CARD_BIN_A    0x1000
+#define CARD_BIN_2    0x2000
+#define CARD_BIN_r    0x4000
+#define CARD_BIN_R    0x8000
 
 #define CARD_SET_LENGTH     54
 
@@ -64,6 +123,9 @@ extern "C" {
 #define CARD_IS_BLACK(x)    (CARD_SUIT(x) == CARD_SUIT_CLUB || CARD_SUIT(x) == CARD_SUIT_SPADE)
 
 #define Card_Make(suit, rank) ((suit)|(rank))
+
+#define CARD_RANK_BEG   CARD_RANK_3
+#define CARD_RANK_END   (CARD_RANK_R + 1)
 
 /*
  * ************************************************************
